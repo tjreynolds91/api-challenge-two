1215b924649247ba76326d0d3c4ad4d80926068b
tjreynolds91 <chinchilla@babies.com>
2020-10-05 16:55:04 -0400

    10/5

--- scripts/index.js
+++ scripts/index.js
@@ -4,48 +4,81 @@ const baseURL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos
 const key = 'y3DUrejxMPFNOGtCKPo36l4yaBgHPoNb8kInsRLw';
 const url = baseURL + key;
 const secondURL = 'https://api.nasa.gov/planetary/apod?api_key=';
-const urlTwo = secondURL + key;
 
 //Links for fetched images
 const jumboBack = document.querySelector('.jumbotron-fluid');
 const cardLeft = document.querySelector('.cardLeft');
 const cardMiddle = document.querySelector('.cardMiddle');
-const cardRight = document.querySelector('cardRight');
+const cardRight = document.querySelector('.cardRight');
 console.log('cardLeft :', cardLeft);
 console.log('cardRight :', cardRight);
 console.log('cardMdiddle :', cardMiddle);
 console.log('jumbotron-fluid :', jumboBack);
+const button = document.querySelector('.randomizer');
+console.log('randomizer :', button);
+
+button.addEventListener('click', randomizer);
 
 
 
-// Fetch Functions
+// Fetch Functions from url for left and middle cards
 fetch(url)
   .then(function(response) {
         return response.json();
     })
     .then(function(myJson) {
         console.log(myJson)
-        
+        cardLeft.src=myJson.photos[599].img_src;   // Display image to cardLeft from 'const = url'
+        cardMiddle.src=myJson.photos[200].img_src;  // Display image to cardMiddle from 'const = url'
+
       
-})
+});
 
-fetch(urlTwo)
-  .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        console.log(myJson)
+
+// Function declaring the random year number to urlTwo api.
+function randomizer(){
+  
+  // const urlTwo = secondURL + key + '&date='+ date;
+
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  getRandomIntInclusive(1,30);
+  let date = getRandomIntInclusive(1,30);
+  const urlTwo = secondURL + key + '&date=2020' + '-9-' + date;
+
+
+
+    
+       
+
+
+
+  
+
+    // Fetch Functions from urlTwo for right card to randomize each click of the button.
+  fetch(urlTwo)
+    .then(function(response) {
+          return response.json();
+      })
+      .then(function(myJson) {
+          console.log(myJson);
+          console.log(myJson.hdurl);
+          if(myJson.hdurl) {
+            cardRight.src=myJson.hdurl    // Display image to cardRight  from 'const = urlTwo'
+          }
+          else {
+            return randomizer();
+          }
+
+          
+          // else()
+
+          
         
-      
-})
+  })
+};
+randomizer(); // calls the randomizer function to be ablt to randomize the picture pulled from api by date.
 
-// Display image to jumbtron background from 'const = urlTwo'
-
-
-// Display image to cardLeft from 'const = url'
-
-
-// Display image to cardMiddle 'const = url'
-
-
-// Display image to cardRight  'const = url'
